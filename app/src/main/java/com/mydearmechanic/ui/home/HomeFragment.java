6e5fe1ecdb80da7b80ced8647a8217a309320deb
AashaDass <aasha1482000@gmail.com>
2023-02-22 15:05:28 +0530

    first commit

--- app/src/main/java/com/mydearmechanic/ui/home/HomeFragment.java
+++ app/src/main/java/com/mydearmechanic/ui/home/HomeFragment.java
@@ -1,23 +1,32 @@
 package com.mydearmechanic.ui.home;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.mydearmechanic.MainActivity;
+import com.mydearmechanic.R;
 import com.mydearmechanic.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    Activity context;
 
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        context=getActivity();
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
@@ -28,10 +37,38 @@ public class HomeFragment extends Fragment {
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+    public  void onStart()
+    {
+        super.onStart();
+        Spinner spinner= context.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context, R.array.quantity, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+
+        super.onStart();
+        Spinner spinner1= context.findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(context, R.array.district, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner1.setAdapter(adapter1);
+
+        super.onStart();
+        Spinner spinner2= context.findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(context, R.array.state, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner2.setAdapter(adapter2);
+
+        super.onStart();
+        Spinner spinner3= context.findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(context, R.array.pincode, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner3.setAdapter(adapter3);
+    }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+
+
     }
 }
